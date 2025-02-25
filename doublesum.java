b8f0af4eddcc74b3bb4a6b50ec45e963e91e8d8c
GEETHAPRIYA S L <s.l.geethapriya8888@gmail.com>
2025-02-25 20:43:42 +0530

    Create doublesum.java

---
+++ doublesum.java
@@ -0,0 +1,21 @@
+public class FractionSum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+      
+        System.out.print("Enter first decimal number: ");
+        double num1 = input.nextDouble();
+        
+        System.out.print("Enter second decimal number: ");
+        double num2 = input.nextDouble();
+        frac1 = num1 - Math.floor(num1)
+        frac2 = num2 - Math.floor(num2)
+        sum = frac1 + frac2
+        System.out.println(sum)
+
+          }
+}
+          
+
+
+        
